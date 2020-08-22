2ab757ee6a7a1fbb872731a005d34ea1b0aca4e0
Peter Nagy <nagy.peter.home@gmail.com>
2020-08-22 21:57:42 +0200

    Build on linux

--- template-auth-service/src/main/java/hu/perit/template/authservice/Application.java
+++ template-auth-service/src/main/java/hu/perit/template/authservice/Application.java
@@ -35,7 +35,6 @@ import org.springframework.context.annotation.Profile;
 @Log4j
 public class Application {
 
-
     public static void main(String[] args) { // NOSONAR
         SpringApplication application = new SpringApplication(Application.class);
         application.addListeners(new EnvironmentPostProcessor());
