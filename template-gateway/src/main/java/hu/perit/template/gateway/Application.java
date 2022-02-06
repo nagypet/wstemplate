2d055fd6d96fbc61c5751f385b537456d823a2ca
Peter Nagy <peter.nagy@perit.hu>
2022-02-06 12:20:28 +0100

    template gateway runs on http, without discovery

---
+++ template-gateway/src/main/java/hu/perit/template/gateway/Application.java
@@ -0,0 +1,13 @@
+package hu.perit.template.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ template-gateway/src/test/java/hu/perit/template/gateway/ApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.perit.template.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
