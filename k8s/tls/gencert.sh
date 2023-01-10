91b0d81ec215693d83c5af98006b90767cceef9a
Peter Nagy <peter.nagy@perit.hu>
2023-01-10 08:51:31 +0100

    k8s yamls updated

--- k8s/tls/gencert.sh
+++ k8s/tls/gencert.sh
@@ -1,5 +1,7 @@
 KEY_FILE=./key.txt
 CERT_FILE=./cert.txt
+CNAME=wstemplate.k8s-test.perit.hu
+ORG_NAME=perit.hu
 HOST1=auth-service.wstemplate.k8s-test.perit.hu
 HOST2=scalable-service.wstemplate.k8s-test.perit.hu
-openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ${KEY_FILE} -out ${CERT_FILE} -subj "/CN=${HOST1}/O=${HOST1}" -addext "subjectAltName = DNS:${HOST1},DNS:${HOST2}"
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ${KEY_FILE} -out ${CERT_FILE} -subj "/CN=${CNAME}/O=${ORG_NAME}" -addext "subjectAltName = DNS:${HOST1},DNS:${HOST2}"
