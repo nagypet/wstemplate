c65563a63d56c8119cebb9ecc3f5dba1243f3924
Peter Nagy <peter.nagy@perit.hu>
2023-02-05 12:47:52 +0100

    perit.hu -> perit.local

--- k8s/tls/gencert.sh
+++ k8s/tls/gencert.sh
@@ -1,7 +1,7 @@
 KEY_FILE=./key.txt
 CERT_FILE=./cert.txt
-CNAME=wstemplate.k8s-test.perit.hu
+CNAME=wstemplate.k8s-test.perit.local
 ORG_NAME=perit.hu
-HOST1=auth-service.wstemplate.k8s-test.perit.hu
-HOST2=scalable-service.wstemplate.k8s-test.perit.hu
+HOST1=auth-service.wstemplate.k8s-test.perit.local
+HOST2=scalable-service.wstemplate.k8s-test.perit.local
 openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ${KEY_FILE} -out ${CERT_FILE} -subj "/CN=${CNAME}/O=${ORG_NAME}" -addext "subjectAltName = DNS:${HOST1},DNS:${HOST2}"
